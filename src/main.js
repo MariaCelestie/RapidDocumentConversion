83b8bdadb67bfcbb9607c417bc6604e65dc42b5d
c0827907 <96276876+c0827907@users.noreply.github.com>
2021-12-16 23:18:51 -0500

    Delete src directory

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import axios from 'axios';
-
-
-const app = createApp(App)
-app.mount('#app')
-app.config.globalProperties.$axios = axios;
